f25110877aaf5d5f4f4cd28722dd227f8f775250
GauravSaini144 <149235914+GauravSaini144@users.noreply.github.com>
2024-10-07 18:01:59 +0530

    Updating styling

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,18 +1,27 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import {Routes, Route} from "react-router-dom"
 import './App.css'
 import Home from './Home'
-
+import Splash from './components/Splash'
 function App() {
-  const [count, setCount] = useState(0)
+  const [visible, setVisible]=useState(true);
+  useEffect(()=>{
+
+      const timer=setTimeout(()=>setVisible(false),8000);
+      return()=>clearTimeout(timer);
+  
+  },[]);
 
   return (
-    <>
+    <>{ visible===true ?<>
+    <Splash/>
+    </>:
     <Routes>
+      
       <Route path="/" element={<Home/>} />
 
-      </Routes>
-    </>
+      </Routes>}
+</>
   )
 }
 
---
+++ Portfolio/src/components/Splash.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import "./Splash.css"
+function Splash() {
+
+   
+      return (
+    <>
+
+    
+        <div className='splash'>
+         <h1>Gaurav Saini</h1>
+
+        </div>
+    
+    
+    </>
+  )
+}
+
+export default Splash
